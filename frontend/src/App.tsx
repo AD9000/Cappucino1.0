39cb7bfa3b94a3232d11d969d219e1144ec868cb
AD9000 <damle.atharv16@gmail.com>
2020-01-01 01:19:43 +1100

    Created and centered board

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './chessFont.css'
+import { Board } from './Board';
+import { Grid, Container, Paper } from '@material-ui/core';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p style={{fontFamily:'Chess', color: 'green'}}>The quick brown fox jumps over the lazy dog</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  render() {
+    return (
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: '100vh' }}
+      >
+        <Grid item xs={3}>
+          <Paper elevation={3}>
+            <Board />
+          </Paper>
+        </Grid>
+      </Grid>
+    );
+  }
 }
-
-export default App;
--- frontend/src/Board.tsx
+++ frontend/src/Board.tsx
@@ -1,20 +1,44 @@
-import React from "react";
-import { Grid, Paper, Table, TableHead, TableRow, Tab, TableCell, createStyles, Checkbox, TableBody, makeStyles, Theme } from "@material-ui/core";
-import "./Board.css";
-import './chessFont.css'
-import { withStyles } from "@material-ui/styles";
+import React from 'react';
+import {
+  Grid,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  Tab,
+  TableCell,
+  createStyles,
+  Checkbox,
+  TableBody,
+  makeStyles,
+  Theme
+} from '@material-ui/core';
+import './Board.css';
+import './chessFont.css';
+import { withStyles } from '@material-ui/styles';
 
 export class Board extends React.Component {
   state = {
     board: [
-      ['r', 'n', 'b', '']
+      ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
+      ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+      ['', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', ''],
+      ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+      ['r', 'n', 'b', 'k', 'q', 'b', 'n', 'r']
     ]
-  }
-
-
+  };
 
   render() {
-    function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
+    function createData(
+      name: string,
+      calories: number,
+      fat: number,
+      carbs: number,
+      protein: number
+    ) {
       return { name, calories, fat, carbs, protein };
     }
 
@@ -23,7 +47,7 @@ export class Board extends React.Component {
       createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
       createData('Eclair', 262, 16.0, 24, 6.0),
       createData('Cupcake', 305, 3.7, 67, 4.3),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
+      createData('Gingerbread', 356, 16.0, 49, 3.9)
     ];
 
     const x = withStyles({
@@ -36,8 +60,7 @@ export class Board extends React.Component {
         textAlign: 'center',
         fontFamily: 'Chess'
       }
-    })
-
+    });
 
     const d = {
       lightbackground: {
@@ -46,7 +69,6 @@ export class Board extends React.Component {
         fontFamily: 'Chess'
       }
     };
-
 
     return (
       // <Grid container className="Hi">
@@ -65,35 +87,91 @@ export class Board extends React.Component {
       // <Table>
       //   hi how are you
       // </Table>
-      <Paper >
-        <Table>
-          <TableHead>
-            <TableRow>
-              {/* <TableCell style={d.lightbackground}>r</TableCell> */}
-              <TableCell className='darkbackground' style={{ fontFamily: 'Chess', textAlign: 'center' }}>n</TableCell>
-              <TableCell align='center' className='lightbackground' style={{ fontFamily: 'Chess' }}>r</TableCell>
-              <TableCell className='lightbackground' style={{ fontFamily: 'Chess' }}>r</TableCell>
-              <TableCell className='lightbackground' style={{ fontFamily: 'Chess' }}>r</TableCell>
-              <TableCell className='lightbackground' style={{ fontFamily: 'Chess' }}>r</TableCell>
-              <TableCell className='lightbackground' style={{ fontFamily: 'Chess' }}>r</TableCell>
-              {/* <TableCell className={d.lightbackground}>r</TableCell> */}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {/* {rows.map(row => (
-              <TableRow key={row.name}>
-                <TableCell component="th" scope="row">
-                  {row.name}
+      // <Table>
+      //   <TableHead>
+      //     <TableRow>
+      //       {/* <TableCell style={d.lightbackground}>r</TableCell> */}
+      //       <TableCell
+      //         className="darkbackground"
+      //         style={{ fontFamily: 'Chess', textAlign: 'center' }}
+      //       >
+      //         the quick brown fox jumped over the lazy dog
+      //       </TableCell>
+      //       <TableCell
+      //         align="center"
+      //         className="lightbackground"
+      //         style={{ fontFamily: 'Chess' }}
+      //       >
+      //         r
+      //       </TableCell>
+      //       <TableCell
+      //         className="lightbackground"
+      //         style={{ fontFamily: 'Chess' }}
+      //       >
+      //         r
+      //       </TableCell>
+      //       <TableCell
+      //         className="lightbackground"
+      //         style={{ fontFamily: 'Chess' }}
+      //       >
+      //         r
+      //       </TableCell>
+      //       <TableCell
+      //         className="lightbackground"
+      //         style={{ fontFamily: 'Chess' }}
+      //       >
+      //         r
+      //       </TableCell>
+      //       <TableCell
+      //         className="lightbackground"
+      //         style={{ fontFamily: 'Chess' }}
+      //       >
+      //         r
+      //       </TableCell>
+      //       {/* <TableCell className={d.lightbackground}>r</TableCell> */}
+      //     </TableRow>
+      //   </TableHead>
+      //   <TableBody>
+      //     {/* {rows.map(row => (
+      //         <TableRow key={row.name}>
+      //           <TableCell component="th" scope="row">
+      //             {row.name}
+      //           </TableCell>
+      //           <TableCell align="right">{row.calories}</TableCell>
+      //           <TableCell align="right">{row.fat}</TableCell>
+      //           <TableCell align="right">{row.carbs}</TableCell>
+      //           <TableCell align="right">{row.protein}</TableCell>
+      //         </TableRow>
+      //       ))} */}
+      //   </TableBody>
+      // </Table>
+
+      <Table>
+        <TableBody>
+          {this.state.board.map((row, rIndex) => (
+            <TableRow key={rIndex} style={{ height: '50px' }}>
+              {row.map((cell, index) => (
+                <TableCell
+                  key={index}
+                  className={
+                    (rIndex % 2 == 0 && index % 2 === 0) ||
+                    (index % 2 !== 0 && rIndex % 2 !== 0)
+                      ? 'lightbackground'
+                      : 'darkbackground'
+                  }
+                  style={{
+                    fontFamily: 'Chess',
+                    textAlign: 'center',
+                    width: '50px'
+                  }}
+                >
+                  {cell}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
-              </TableRow>
-            ))} */}
-          </TableBody>
-        </Table>
-      </Paper >
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     );
   }
 }
